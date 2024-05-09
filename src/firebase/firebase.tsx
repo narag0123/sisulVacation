d1fc9c57dee98318fa7876bf959a11c7a21d8f17
narag0123 <narag0123@gmail.com>
2024-05-09 20:19:14 +0900

    rollback

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -10,25 +10,25 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // apiKey: "AIzaSyCTwMc2ShiHt4o933L-q2NeCOeUWioe--Q",
-    // authDomain: "sdc-new-calendar-vacation.firebaseapp.com",
-    // projectId: "sdc-new-calendar-vacation",
-    // storageBucket: "sdc-new-calendar-vacation.appspot.com",
-    // messagingSenderId: "129344356801",
-    // appId: "1:129344356801:web:d6660765fa0c7b968d2f40",
-    // measurementId: "G-YDQN36SFN4",
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-    projectId:
-        process.env.NEXT_PUBLIC_FIREBASE_PROTECTED_ID,
-    storageBucket:
-        process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:
-        process.env
-            .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId:
-        process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyCTwMc2ShiHt4o933L-q2NeCOeUWioe--Q",
+    authDomain: "sdc-new-calendar-vacation.firebaseapp.com",
+    projectId: "sdc-new-calendar-vacation",
+    storageBucket: "sdc-new-calendar-vacation.appspot.com",
+    messagingSenderId: "129344356801",
+    appId: "1:129344356801:web:d6660765fa0c7b968d2f40",
+    measurementId: "G-YDQN36SFN4",
+    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    // projectId:
+    //     process.env.NEXT_PUBLIC_FIREBASE_PROTECTED_ID,
+    // storageBucket:
+    //     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId:
+    //     process.env
+    //         .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    // measurementId:
+    //     process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
