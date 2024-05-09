c0d0efa5a702169ed631e44b3e78b736248fda1d
narag0123 <narag0123@gmail.com>
2024-05-09 20:35:10 +0900

    env added fin

--- src/app/(View)/(Pages)/main_page.tsx
+++ src/app/(View)/(Pages)/main_page.tsx
@@ -25,50 +25,6 @@ const MainPage = () => {
         setIsFilter,
     } = context;
 
-    // const touchStartY = useRef(0);
-
-    // // Pull-to-Refresh 구현
-    // useEffect(() => {
-    //     // 터치 시작 이벤트 핸들러
-    //     const handleTouchStart = (event: TouchEvent) => {
-    //         touchStartY.current = event.touches[0].clientY;
-    //     };
-
-    //     // 터치 이동 이벤트 핸들러
-    //     const handleTouchMove = (event: TouchEvent) => {
-    //         const currentY = event.touches[0].clientY;
-    //         const diffY = currentY - touchStartY.current;
-
-    //         // 사용자가 페이지 맨 위에 있고, 터치를 아래로 당긴 정도가 임계값을 넘으면 페이지를 새로고침
-    //         if (window.scrollY === 0 && diffY > 300) {
-    //             const confirmed = confirm("refresh?");
-    //             confirmed && window.location.reload();
-    //         }
-    //     };
-
-    //     // 터치 이벤트 리스너 추가
-    //     window.addEventListener(
-    //         "touchstart",
-    //         handleTouchStart
-    //     );
-    //     window.addEventListener(
-    //         "touchmove",
-    //         handleTouchMove
-    //     );
-
-    //     // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    //     return () => {
-    //         window.removeEventListener(
-    //             "touchstart",
-    //             handleTouchStart
-    //         );
-    //         window.removeEventListener(
-    //             "touchmove",
-    //             handleTouchMove
-    //         );
-    //     };
-    // }, []);
-
     return (
         <>
             <LocalizationProvider
